57a79527b4413df72c93bb60a3c4b1911fa7c858
bima susila mukti <kepo.cok@gmail.com>
2020-04-15 16:53:12 +0700

    add API

---
+++ app/src/main/java/com/bima/maknews/api/NewsApi.java
@@ -0,0 +1,10 @@
+package com.bima.maknews.api;
+
+public class NewsApi {
+    public static String GET_TOP_HEADLINES = "https://newsapi.org/v2/top-headlines?country=id&apiKey=82e7f2eddff349c496f73de11025ee3c";
+    public static String GET_CATEGORY_SPORTS = "https://newsapi.org/v2/top-headlines?country=id&category=sports&apiKey=82e7f2eddff349c496f73de11025ee3c";
+    public static String GET_CATEGORY_TECHNOLOGY = "https://newsapi.org/v2/top-headlines?country=id&category=technology&apiKey=82e7f2eddff349c496f73de11025ee3c";
+    public static String GET_CATEGORY_BUSINESS = "https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=82e7f2eddff349c496f73de11025ee3c";
+    public static String GET_CATEGORY_HEALTH = "https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=82e7f2eddff349c496f73de11025ee3c";
+    public static String GET_CATEGORY_ENTERTAINMENT = "https://newsapi.org/v2/top-headlines?country=id&category=entertainment&apiKey=82e7f2eddff349c496f73de11025ee3c";
+}
